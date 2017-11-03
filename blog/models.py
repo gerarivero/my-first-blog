1dc36e75573b2a00166374bdaaa3862a3e86cd2c
Gera <gera_riv@hotmail.com>
2017-11-03 18:00:05 -0300

    DjangoGirls - Primer Commit

---
+++ blog/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+from django.utils import timezone
+
+class Post(models.Model):
+	autor= models.ForeignKey('auth.User')
+	titulo= models.CharField(max_length=200)
+	texto= models.TextField()
+	fecha_creacion= models.DateTimeField(default=timezone.now)	
+	fecha_publicacion= models.DateTimeField(blank= True, null=True)
+
+	def publicar(self):
+		self.fecha_publicacion= timezone.now()
+
+	def __str__(self):
+		return self.titulo
